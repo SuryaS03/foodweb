30aad88afb9c23feee2c9800885758afa85bf180
suryas03 <suryakgm7203@gmail.com>
2024-05-06 16:04:54 +0530

    changes 4.5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
-
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Route,Routes}from 'react-router-dom'
+import Home from './pages/Home';
 function App() {
-  return (
-  <h1>Startted react</h1>
+  return (<div className='App'>
+<Router>
+  
+  <Navbar/>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+  </Routes>
+  </Router>
+  </div>
+  
   );
+  
 }
 
 export default App;
